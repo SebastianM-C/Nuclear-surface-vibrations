d2e2177f9efd9e6a3fa1809968408e119c29ec55
Sebastian <m.c.sebastian95@gmail.com>
2018-04-09 02:51:32 +0300

    Add Regex parameter

--- src/quantum/utils.jl
+++ src/quantum/utils.jl
@@ -7,20 +7,21 @@ export concat, files
 using DataFrames, CSV
 
 """
-    files(name; location="quantum")
+    files(name; kwargs)
 
 Return all the files in the output folder for the given `location` that
-have the given `name`.
+have the given `name`. Optionally specify the Regex for the output folders.
 
 ## Arguments
 - `name`: the name of the files
 - `location="quantum"`: the location in the output folder. Can be `"classical"`
 or `"qunatum"`
+- `re=r"n[0-9]+-b[0-9]+\.[0-9]+-d[0-9]+\.[0-9]+"`: the Regex for the output
+folders
 
 """
-function files(name; location="quantum")
+function files(name; location="quantum", re=r"n[0-9]+-b[0-9]+\.[0-9]+-d[0-9]+\.[0-9]+")
     prefix = "../../output/$location/"
-    re = r"n[0-9]+-b[0-9]+\.[0-9]+-d[0-9]+\.[0-9]+"
     has_data(f) = any(contains.(readdir(joinpath(prefix, f)), name))
     folders = filter(f->ismatch(re, f) && has_data(f), readdir(prefix))
 
