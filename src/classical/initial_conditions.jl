02e405c570f29cb78366105de86baaca2c9240f0
sebastian <m.c.sebastian95@gmail.com>
2018-08-26 14:33:50 +0300

    Add another 2 algorithms for initial conditions
    These 2 methods use initial conditions on the Poincare section

--- src/classical/initial_conditions.jl
+++ src/classical/initial_conditions.jl
@@ -4,10 +4,13 @@ module InitialConditions
 export generateInitialConditions
 
 using Logging
+using Random
 using NLsolve
 using NLopt
+using Roots
 using Plots
 using DataFrames, CSV
+using GeometricalPredicates
 
 include("hamiltonian.jl")
 using .Hamiltonian
@@ -62,12 +65,14 @@ end
 
 function filter_NaNs!(q0, p0, n, m)
     nan_no = count(isnan.(q0))
-    nan_no % 2 != 0 && @error "Can't remove NaN lines"
-    not_NaN = .!isnan.(q0)
-    q0 = reshape(q0[not_NaN], (n*m - Int(nan_no / 2), 2))
-    p0 = reshape(p0[not_NaN], (n*m - Int(nan_no / 2), 2))
-    N = n*m - Int(nan_no / 2)
-    nan_no != 0 && @info "Found $nan_no NaNs and removed $(n*m-N) initial conditions."
+    if nan_no > 0
+        nan_no % 2 != 0 && @error "Can't remove NaN lines" nan_no
+        not_NaN = .!isnan.(q0)
+        q0 = reshape(q0[not_NaN], (n*m - nan_no ÷ 2, 2))
+        p0 = reshape(p0[not_NaN], (n*m - nan_no ÷ 2, 2))
+        @info "Found $nan_no NaNs leaving $(n*m-nan_no÷2) initial conditions."
+    end
+    N = n*m - nan_no ÷ 2
     N == 0 && @error "All initial conditions are invalid!"
 
     return q0, p0, N
@@ -136,6 +141,74 @@ function _generateInitialConditions(E, n=15, m=15; params=(A=1, B=0.55, D=0.4);
     maxerr = maximum(abs(H(p0[i,:], q0[i,:], params) - E) for i = 1:N)
     @info "The maximum error for the initial conditions is $maxerr"
     return q0, p0, N
+end
+
+"""
+    tointerval(x, ex)
+
+Resale and move `x` to the interval ``[1, 2)`` given the extrema `ex` of the original interval.
+This is required by the algorithms in `GeometricalPredicates`.
+"""
+function tointerval(x, ex)
+    (x - ex[1]) / (ex[2] - ex[1] + 1e-14) + 1
+end
+
+function tointerval(points, ex, ey)
+    [Point(tointerval(p._x, ex), tointerval(p._y, ey)) for p in points]
+end
+
+"""
+    frominterval(x, ex)
+
+Move `x` to the original interval with the extrema `ex` from ``[1, 2)``.
+See also [`tointerval`](@ref).
+"""
+function frominterval(x, ex)
+    (x - 1) * (ex[2] - ex[1] + 1e-14) + ex[1]
+end
+
+function frominterval(points, ex, ey)
+    [frominterval(p._x, ex) for p in points], [frominterval(p._y, ey) for p in points]
+end
+
+function phase_space_border(E, n=1000; param=(A=1, B=0.55, D=0.4))
+    p = range(0, stop=√(2 * param.A * E), length=n)
+    q0 = E < 1e5 ? 0 : 10
+    q = [find_zero(q->H([0,pᵢ], [0,q], param) - E, q0) for pᵢ in p]
+    points = Point.(q, p) ∪ Point.(-q[end-1:-1:1], p[end-1:-1:1]) ∪
+        Point.(-q[2:end], -p[2:end]) ∪ Point.(q[end-1:-1:2], -p[end-1:-1:2])
+    ex = extrema(p._x for p in points)
+    ey = extrema(p._y for p in points)
+    Polygon(tointerval(points, ex, ey)...), ex, ey
+end
+
+Random.rand(rng::AbstractRNG, ::Random.SamplerType{Point2D}) = Point((rand(rng) + 1), (rand(rng) + 1))
+
+function _generateInitialConditions(E, n; param=(A=1, B=0.55, D=0.4),
+        border_n=1000, alg::Val{:poincare_rand})
+    border, ex, ey = phase_space_border(E, border_n, param=param)
+    counter = 0
+    points = Vector{Point2D}()
+    while counter < n
+        p = rand(Point2D)
+        inpolygon(border, p) && (push!(points, p); counter += 1)
+    end
+    frominterval(points, ex, ey)
+end
+
+function _generateInitialConditions(E, n, m; param=(A=1, B=0.55, D=0.4),
+        border_n=1000, alg::Val{:poincare_uniform})
+    border, ex, ey = phase_space_border(E, border_n, param=param)
+    counter = 0
+    points = Vector{Point2D}()
+    it_grid = Iterators.product(range(1,stop=2-1e-14,length=n),
+        range(1,stop=2-1e-14,length=m))
+    grid = [Point(x, y) for (x, y) ∈ it_grid]
+    for p in grid
+        inpolygon(border, p) && (push!(points, p); counter += 1)
+    end
+    @debug "Generated $count initial conditions."
+    frominterval(points, ex, ey)
 end
 
 end  # module InitialConditions
