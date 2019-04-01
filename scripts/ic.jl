58b12dfc8952be768d77f7db434847f2412d40cd
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2019-04-01 15:38:40 +0300

    addprocs on remote servers

--- scripts/ic.jl
+++ scripts/ic.jl
@@ -1,6 +1,12 @@
 using Distributed
-addprocs(20)
-using NuclearSurfaceVibrations
+
+@progress for i in 1:5
+    addprocs([("cn$i", 40)], exename="/mnt/storage/julia.sh")
+end
+
+# addprocs(20)
+
+@time using NuclearSurfaceVibrations
 using .Classical
 using StorageGraphs
 using LightGraphs
@@ -15,14 +21,15 @@ E = 10.
 q0, p0 = initial_conditions(E, alg=PoincareRand(n=10))
 g = Classical.DataBaseInterface.initalize()
 
-@profiler λmap(E, ic_alg=PoincareRand(n=7), params=PhysicalParameters(B=0.20))
+@profiler λmap(E, ic_alg=PoincareRand(n=100), params=PhysicalParameters(B=0.55))
 
 with_logger(dbg) do
-    λmap(E, ic_alg=PoincareRand(n=100), params=PhysicalParameters(B=0.2), alg=DynSys())
+    @time λmap(E, ic_alg=PoincareRand(n=100), params=PhysicalParameters(B=0.2), alg=DynSys())
 end
 
-
-
+@progress for E in 10.:10.:1e3
+    λmap(E, ic_alg=PoincareRand(n=500), params=PhysicalParameters(B=0.55), alg=DynSys())
+end
 
 using .Visualizations
 
